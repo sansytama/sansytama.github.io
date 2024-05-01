c5b1dd1c2398d5c5f8a2c72c39390bed951e8ecf
dishes <dishes.a.very.stupid@email_domain.com>
2024-05-01 23:36:16 +0700

    change ignorePatterns

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: null,
     locale: "en-US",
     baseUrl: "sansytama.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "others"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
